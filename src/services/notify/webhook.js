21cb2f1c9d4f36b2aff33ba84bfb2228dcd3f95f
Leon <yyho1527@gmail.com>
2026-07-02 17:31:11 +0800

    Update webhook.js

--- src/services/notify/webhook.js
+++ src/services/notify/webhook.js
@@ -139,7 +139,7 @@ export const webhookChannel = {
     }
 
 const body =  `---
-### 订阅详情
+✨✨**订阅详情**✨✨
 - **类型**: ${requestBody.type}
 - **分类**: ${requestBody.category}
 - **日历类型**: ${requestBody.calendarType}
